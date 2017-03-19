58d99f019c0024c806b060b2982f7851a79358f8
Witoo Harianto <witoo@plimble.com>
2017-03-19 13:28:49 +0700

    update

--- badge/service.go
+++ badge/service.go
@@ -2,16 +2,19 @@ package badge
 
 import "fmt"
 
+// Service interface
 type Service interface {
 	Badge(num float64, status int) string
 }
 
 type service struct{}
 
-func NewService() *service {
+// NewService func
+func NewService() Service {
 	return &service{}
 }
 
+// Badge generate url badge
 func (s *service) Badge(num float64, status int) string {
 	switch status {
 	case 0:
@@ -23,9 +26,8 @@ func (s *service) Badge(num float64, status int) string {
 			return fmt.Sprintf("https://img.shields.io/badge/coverage-%.1f%%25-orange.svg?style=flat", num)
 		} else if num < 75.0 {
 			return fmt.Sprintf("https://img.shields.io/badge/coverage-%.1f%%25-green.svg?style=flat", num)
-		} else {
-			return fmt.Sprintf("https://img.shields.io/badge/coverage-%.1f%%25-brightgreen.svg?style=flat", num)
 		}
+		return fmt.Sprintf("https://img.shields.io/badge/coverage-%.1f%%25-brightgreen.svg?style=flat", num)
 	case 2:
 		return fmt.Sprintf("https://img.shields.io/badge/coverage-error-red.svg?style=flat")
 	default:
